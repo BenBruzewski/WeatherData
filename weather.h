23339ed23ca22ecae20f806da6f41727dfc7459e
Ben Bruzewski <95446475+BenBruzewski@users.noreply.github.com>
2021-12-21 15:32:32 -0600

    Create weather.h
    This is one of many weather.h files. As long as the formatting is kept, any data can be entered into these arrays and tested with the MSP430FG4618.

---
+++ weather.h
@@ -0,0 +1,12 @@
+/*
+;Description: A user-made header file based on data retrieved at visualcrossing.com
+;Input: ~
+;Output: ~
+;Author: Ben Bruzewski / visualcrossing.com
+;Lab Section: Section 01
+;Date: 11/19/2021
+*/
+float WeatherF[]= { 52.4, 0.07 }; // size 2. Should not need blank fillers
+float WeatherC[]= {'5','2','.','4', ' ','0','.','0','7'}; // size 9 always, fill blanks with ' '
+float WeatherI[] = {5,2, 10,0,10,10}; // Size 6 always, fill blanks with 10 as Char_Map[10] = LCD_D
+which is an underscore (filler space)
